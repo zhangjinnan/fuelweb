b0341fefbdea476955796ab0da935734353123aa
e0ne <ikolodyazhny@mirantis.com>
2013-05-14 11:47:48 +0300

    Fix typo

--- nailgun/static/js/views/cluster_page_tabs/nodes_tab.js
+++ nailgun/static/js/views/cluster_page_tabs/nodes_tab.js
@@ -842,7 +842,6 @@ function(models, commonViews, dialogViews, nodesTabSummaryTemplate, editNodesScr
             this.node = this.model.get('nodes').get(this.screenOptions[0]);
             if (this.node) {
                 this.interfaces = new models.Interfaces();
-
                 this.interfaces.fetch({
                     url: _.result(this.node, 'url') + '/attributes/interfaces'
                 }).done(_.bind(function(){
@@ -872,10 +871,10 @@ function(models, commonViews, dialogViews, nodesTabSummaryTemplate, editNodesScr
                         var obj = $(event.target);
                         obj.children(".network-help-message").addClass("hide");
                         var ifcName = obj.parent().parent().children(".network-box-name").html();
-                        physicalIfc = _.find(this.interfaces.models,
+                        var physicalIfc = _.find(this.interfaces.models,
                                 function(ifc){ return ifc.get("name")==ifcName });
                         this.addNetwork(physicalIfc, ifNetwork);
-                        this.renderNetworks(ifc);
+                        this.renderNetworks(physicalIfc);
                     }, this),
                     remove: _.bind(function(event, ui){
                         var obj = $(event.target);
@@ -920,4 +919,5 @@ function(models, commonViews, dialogViews, nodesTabSummaryTemplate, editNodesScr
 
     return NodesTab;
 });
+
 
