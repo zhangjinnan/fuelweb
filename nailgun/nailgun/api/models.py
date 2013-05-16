5470192fe9b0b5a16f78fb3d6eaab86c6323dd65
Andrey Danin <gcon.monolake@gmail.com>
2013-05-16 19:51:44 +0400

    [nailgun] Fix NICs update when nodes add to/remove from cluster.

--- nailgun/nailgun/api/models.py
+++ nailgun/nailgun/api/models.py
@@ -498,7 +498,7 @@ class L2Topology(Base):
     id = Column(Integer, primary_key=True)
     network_id = Column(
         Integer,
-        ForeignKey('networks.id', ondelete="CASCADE"),
+        ForeignKey('network_groups.id', ondelete="CASCADE"),
         nullable=False
     )
 
@@ -525,7 +525,7 @@ class AllowedNetworks(Base):
     id = Column(Integer, primary_key=True)
     network_id = Column(
         Integer,
-        ForeignKey('networks.id', ondelete="CASCADE"),
+        ForeignKey('network_groups.id', ondelete="CASCADE"),
         nullable=False
     )
     interface_id = Column(
@@ -540,7 +540,7 @@ class NetworkAssignment(Base):
     id = Column(Integer, primary_key=True)
     network_id = Column(
         Integer,
-        ForeignKey('networks.id', ondelete="CASCADE"),
+        ForeignKey('network_groups.id', ondelete="CASCADE"),
         nullable=False
     )
     interface_id = Column(
@@ -563,12 +563,11 @@ class NodeNICInterface(Base):
     max_speed = Column(Integer)
     current_speed = Column(Integer)
     allowed_networks = relationship(
-        "Network",
+        "NetworkGroup",
         secondary=AllowedNetworks.__table__,
     )
     assigned_networks = relationship(
-        "Network",
+        "NetworkGroup",
         secondary=NetworkAssignment.__table__,
     )
-
 
