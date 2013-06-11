617f01c72d36f6dc3b3972a2eac5abd8dcbb47c1
mattymo <raytrac3r@gmail.com>
2013-06-11 16:28:24 +0400

    Update site.pp

--- puppet/rpmcache/examples/site.pp
+++ puppet/rpmcache/examples/site.pp
@@ -5,8 +5,8 @@ node default {
     rh_username => "your_rh_username",
     rh_password => "your_rh_password",
     rh_base_channels => "rhel-6-server-rpms rhel-6-server-optional-rpms rhel-lb-for-rhel-6-server-rpms rhel-rs-for-rhel-6-server-rpms rhel-ha-for-rhel-6-server-rpms rhel-server-ost-6-folsom-rpms",
-    rh_openstack_channel => "rhel-server-ost-6-folsom-rpms",
-    #rh_openstack_channel => "rhel-server-ost-6-3-rpms",
+    #rh_openstack_channel => "rhel-server-ost-6-folsom-rpms",
+    rh_openstack_channel => "rhel-server-ost-6-3-rpms",
     use_satellite => false,
     sat_hostname => undef,
     activation_key => undef
