cc6bc6a3bb4ab7eb4a0952244a71dc2cf5e1a1b7
e0ne <e0ne@e0ne.info>
2013-06-17 17:09:05 +0400

    Show VLAN name on network validation

--- nailgun/nailgun/task/task.py
+++ nailgun/nailgun/task/task.py
@@ -549,6 +549,7 @@ class VerifyNetworksTask(object):
     def execute(self, task, data):
         task_uuid = task.uuid
         nodes = []
+        print '====================', data
         for n in task.cluster.nodes:
             node_json = {'uid': n.id, 'networks': []}
             for nic in n.interfaces:
@@ -562,7 +563,8 @@ class VerifyNetworksTask(object):
                         lambda i: i['name'] == ng.name,
                         data
                     )[0]
-                    vlans.extend(data_ng['vlans'])
+                    print '-!!!_!_!!!_!!!', vlans
+                    vlans.append(data_ng)
                 if not vlans:
                     continue
                 node_json['networks'].append(
