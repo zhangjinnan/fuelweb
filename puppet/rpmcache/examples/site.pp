182fc2d37f06822ab28adb361c9a7ad3a0f66a7e
Dmitry Pyzhov <dpyzhov@mirantis.com>
2013-06-18 20:55:38 +0400

    Fix path in rpmcache

--- puppet/rpmcache/examples/site.pp
+++ puppet/rpmcache/examples/site.pp
@@ -1,7 +1,7 @@
 node default {
   class { "rpmcache":
     releasever => "6Server",
-    pkgdir => "/var/www/nailgun/rhel/6.4/nailgun/x86_64",
+    pkgdir => "/var/www/nailgun/rhel",
     rh_username => "your_rh_username",
     rh_password => "your_rh_password",
     rh_base_channels => "rhel-6-server-rpms rhel-6-server-optional-rpms rhel-lb-for-rhel-6-server-rpms rhel-rs-for-rhel-6-server-rpms rhel-ha-for-rhel-6-server-rpms rhel-server-ost-6-folsom-rpms",
