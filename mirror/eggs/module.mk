024d03c59bb2365dc2beb993e260a7af3d4601dd
Dmitry Pyzhov <dpyzhov@mirantis.com>
2013-07-10 12:55:13 +0400

    Put ostf eggs into iso

--- mirror/eggs/module.mk
+++ mirror/eggs/module.mk
@@ -9,6 +9,9 @@ clean-mirror-eggs:
 	-sudo umount $(BUILD_DIR)/mirror/eggs/SANDBOX/dev
 	sudo rm -rf $(BUILD_DIR)/mirror/eggs
 
+CHROOT_EGGS:=$(filter-out git+ssh%,$(REQUIRED_EGGS))
+NO_CHROOT_EGGS:=$(filter git+ssh%,$(REQUIRED_EGGS))
+
 $(BUILD_DIR)/mirror/eggs/build.done: $(call depv,LOCAL_MIRROR_EGGS)
 $(BUILD_DIR)/mirror/eggs/build.done: $(call depv,REQUIRED_EGGS)
 $(BUILD_DIR)/mirror/eggs/build.done: $(call depv,SANDBOX_PACKAGES)
@@ -17,12 +20,26 @@ $(BUILD_DIR)/mirror/eggs/build.done: export SANDBOX_UP:=$(SANDBOX_UP)
 $(BUILD_DIR)/mirror/eggs/build.done: export SANDBOX_DOWN:=$(SANDBOX_DOWN)
 $(BUILD_DIR)/mirror/eggs/build.done: \
 		$(BUILD_DIR)/mirror/centos/build.done
-
 	mkdir -p $(@D)
-	sudo sh -c "$${SANDBOX_UP}"
 
 	# Creating eggs mirror directory
 	mkdir -p $(LOCAL_MIRROR_EGGS)
+
+	# Clone git+ssh repos and set up list of requirements.
+	# It's ok for public version to fail here, so we ignore errors
+	rm -rf $(BUILD_DIR)/mirror/eggs/git-clone
+	mkdir -p $(BUILD_DIR)/mirror/eggs/git-clone
+	for repo in $(NO_CHROOT_EGGS); do \
+	  (cd $(BUILD_DIR)/mirror/eggs/git-clone && git clone $$repo) || true; \
+	done
+	for repo in $(BUILD_DIR)/mirror/eggs/git-clone/*; do \
+	  (cd $$repo && python setup.py sdist && cp dist/*.tar.gz $(LOCAL_MIRROR_EGGS) ) || true; \
+	done
+	find $(BUILD_DIR)/mirror/eggs/git-clone -wholename '*.egg-info/requires.txt' | xargs sort -u > $(BUILD_DIR)/mirror/eggs/chroot-req-pre.txt
+	echo $(CHROOT_EGGS) >> $(BUILD_DIR)/mirror/eggs/chroot-req-pre.txt
+	sed -e 's/\s\+/\n/g' $(BUILD_DIR)/mirror/eggs/chroot-req-pre.txt | sort -u > $(BUILD_DIR)/mirror/eggs/chroot-req.txt
+
+	sudo sh -c "$${SANDBOX_UP}"
 
 	# Avoiding eggs download duplication.
 	sudo rsync -a --delete $(LOCAL_MIRROR_EGGS) $(SANDBOX)/tmp
@@ -40,12 +57,16 @@ $(BUILD_DIR)/mirror/eggs/build.done: \
 		pip==1.2.1
 
 	# Downloading required pip packages.
-	sudo chroot $(SANDBOX) pip install \
+	# pip cannot download two versions of the package at once,
+	# so we do it in cycle.
+	for dep in `cat $(BUILD_DIR)/mirror/eggs/chroot-req.txt`; do \
+	  sudo chroot $(SANDBOX) pip install \
 		--exists-action=i \
 		--index-url $(MIRROR_EGGS) \
 		--find-links $(MIRROR_EGGS) \
 		--download /tmp/$(notdir $(LOCAL_MIRROR_EGGS)) \
-		$(REQUIRED_EGGS)
+		$$dep; \
+	done
 
 	# # Copying downloaded eggs into eggs mirror
 	rsync -a $(SANDBOX)/tmp/$(notdir $(LOCAL_MIRROR_EGGS))/ $(LOCAL_MIRROR_EGGS)
